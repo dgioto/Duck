4e4435e2188b3578b50a376e1729faf5baeaa635
Alexander Tih <dgioto@gmail.com>
2021-03-04 16:48:08 +0200

    доб. двухпозиционную кнопку, включатель, флажки

--- app/src/main/java/com/example/duck/MainActivity.java
+++ app/src/main/java/com/example/duck/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.duck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //двухпозиционная кнопка
+    public void onToggleButtonClicked(View view){
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on){
+            //Вкл
+        } else {
+            // Выкл
+        }
+    }
+
+    //Выключатель
+    public void onSwitchClicked(View view){
+        boolean on = ((Switch) view).isChecked();
+        if (on){
+            //Вкл
+        } else {
+            // Выкл
+        }
+    }
+
+//    //Флажки
+//    CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox_milk);
+//    boolean checked = checkBox.isChecked();
+//    if(checked){
+//        //действие для установленного флажка
+//    }
+
+    public void onCheckboxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()){
+            case R.id.checkBox_milk:
+                if (checked) {
+                    //Кофе с молоком
+                }else{
+                    //Черный кофе
+                }
+                break;
+            case R.id.checkBox_sugar:
+                if (checked) {
+                    //С сахаром
+                }else{
+                    //Без сахара
+                }
+                break;
+        }
     }
 }
